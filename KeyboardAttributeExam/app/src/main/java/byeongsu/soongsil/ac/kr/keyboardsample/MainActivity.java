b5e5588358e410e63441e5569b7329dda443ce85
ByeongsuPark <byeongsu@soongsil.ac.kr>
2018-05-28 10:57:38 +0900

    Initial Commit

---
+++ KeyboardAttributeExam/app/src/main/java/byeongsu/soongsil/ac/kr/keyboardsample/MainActivity.java
@@ -0,0 +1,13 @@
+package byeongsu.soongsil.ac.kr.keyboardsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
