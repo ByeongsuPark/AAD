d3ec1cbdf21680022067d039078f16ec562508b0
ByeongsuPark <byeongsu@soongsil.ac.kr>
2018-05-29 16:18:24 +0900

    Initial Commit

---
+++ AlertExample/app/src/main/java/byeongsu/soongsil/ac/kr/alertexample/MainActivity.java
@@ -0,0 +1,54 @@
+package byeongsu.soongsil.ac.kr.alertexample;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(this);
+
+        // Set the dialog title
+        myAlertBuilder.setTitle(R.string.alert_title);
+
+        // Set the dialog message
+        myAlertBuilder.setMessage(R.string.alert_message);
+
+        // Add the buttons
+        myAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // User clicke  d the OK button.
+                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // User clicked the Cancel button
+                Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Create and show the dialog.
+        myAlertBuilder.show();
+
+
+    }
+}
