9bdf57bbd35087b61ac4c9241f44a37448299909
ByeongsuPark <byeongsu@soongsil.ac.kr>
2018-06-02 20:39:46 +0900

    Initial Commit

--- DroidCafe/app/src/main/java/byeongsu/soongsil/ac/kr/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/byeongsu/soongsil/ac/kr/droidcafe/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+
+    /**
+     *  display a toast message for the food order
+     *  and starts the food order activity
+     * @param message   Message to display
+     */
+
+    // 아래의 메서드는 실질적으로 MainActivity파일의 어느 위치에 넣어도 상관은 x
+    // 그러나 이런 메서드를 넣을때는 싈질적으로 코드내에서 호출하는 함수 내에 넣는게 좋다고 함.
+    public void showFoodOrder(String message){
+        displayToast(message);
+    }
+
+    /**
+     * Show a message that the donut image was clicked
+     */
+    public void showDonutOrder(View view){
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * Show a message that the ice cream image was clicked
+     */
+    public void showIceCreamOrder(View view){
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    /**
+     * Show a message that the Froyo image was clicked
+     */
+    public void showFroyoOrder(View view){
+        showFoodOrder(getString(R.string.froyo_order_message));
     }
 }
